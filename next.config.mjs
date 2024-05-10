5743dd5865f267a97978added5574a4a5c78832e
salmonco <xkqjsslsek80@naver.com>
2024-05-10 21:47:18 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      //   {
+      //     source: "/api/:path*",
+      //     destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      //   },
+      {
+        source: "/public-api/:path*",
+        destination: "http://apis.data.go.kr/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
