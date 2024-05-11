b84eee5ffc4e9bed277669e49e0ae372b47e8d7f
salmonco <xkqjsslsek80@naver.com>
2024-05-11 21:34:44 +0900

    Test: vercel deploy

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Energy For Me",
-  description: "나를 위한 고효율 제품 검색",
+  description: "나를 위한, 환경을 위한 고효율 제품 검색",
 };
 
 export default function RootLayout({ children }) {
